1cf9fbfe3599d53f4c5ba7e03adb002464aac1c7
Cayden de Wit <cayden@4dewits.com>
2021-01-05 11:28:06 +0200

    dwm stuff

---
+++ .dwm/autostart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+picom --experimental-backend --blur-background --blur-method dual_kawase --blur-strength 10 --backend glx -b -f 
+feh --no-fehbg --bg-scale "/home/cayden/.dwm/back.png" 
+conky
---
+++ README.md
@@ -0,0 +1,3 @@
+# My dots part 2
+
+This time instead of i3, it uses dwm. i3 will now be unmaintained.
